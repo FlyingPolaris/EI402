63e15fa29e0a350628a1ec2de942192fd75a1392
FlyingPolaris <polarisybc@qq.com>
2022-12-05 16:31:13 +0800

    Update camara.py

--- camara.py
+++ camara.py
@@ -1,25 +1,62 @@
-from jetcam.csi_camera import CSICamera
 import cv2
 
-#CSI-0
-camera0 = CSICamera(capture_device=0, width=224, height=224)
-image0 = camera0.read()
-print(image0.shape)
-print(camera0.value.shape)
+# 设置gstreamer管道参数
+def gstreamer_pipeline(
+    capture_width=1280, #摄像头预捕获的图像宽度
+    capture_height=720, #摄像头预捕获的图像高度
+    display_width=1280, #窗口显示的图像宽度
+    display_height=720, #窗口显示的图像高度
+    framerate=60,       #捕获帧率
+    flip_method=0,      #是否旋转图像
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
 
-#CSI-1
-#camera1 = CSICamera(capture_device=1, width=224, height=224)
-#image1 = camera1.read()
-#print(image1.shape)
-#print(camera1.value.shape)
+if __name__ == "__main__":
+    capture_width = 1280
+    capture_height = 720
 
-while 1:
-    image0 = camera0.read()
-    cv2.imshow("CSI Camera0", image0)
+    display_width = 1280
+    display_height = 720
 
-    #image1 = camera1.read()
-    #cv2.imshow("CSI Camera1", image1)
+    framerate = 60			# 帧数
+    flip_method = 0			# 方向
 
-    kk = cv2.waitKey(1)
-    if kk == ord('q'): 
-        break 
+    # 创建管道
+    print(gstreamer_pipeline(capture_width,capture_height,display_width,display_height,framerate,flip_method))
+
+    #管道与视频流绑定
+    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+
+    if cap.isOpened():
+        window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
+
+        # 逐帧显示
+        while cv2.getWindowProperty("CSI Camera", 0) >= 0:
+            ret_val, img = cap.read()
+            cv2.imshow("CSI Camera", img)
+
+            keyCode = cv2.waitKey(30) & 0xFF
+            if keyCode == 27:# ESC键退出
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+    else:
+        print("fail to open camara") 
